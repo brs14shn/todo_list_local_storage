687e3f989643476a1e49b9c4be7196daaf567b44
brs14shn <brsshn292929@gmail.com>
2022-06-11 15:28:38 +0300

    enter was added

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
 //* ======================================================
 //*                     TODO APP
 //* ======================================================
+
+const addBtn=document.getElementById("todo-button");
+const todoInput=document.getElementById("todo-input")
+const todoUl=document.querySelector("#todo-ul")
+
+
+window.onload=function(){
+    todoInput.focus();
+
+
+}
+
+addBtn.addEventListener("click",(e)=>{
+    //console.log(todoInput.value);
+    todoUl.innerHTML +=
+    `<li>
+    <i class="fa fa-check"></i>
+    <p>${todoInput.value}</p>
+    <i class="fa fa-trash"></i>
+    </li>`
+    todoInput.value="";
+   
+
+})
+
+todoInput.addEventListener("keydown",(e)=>{
+   if(e.key=="Enter"){
+    addBtn.click();
+   }
+
+})
